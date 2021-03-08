afe4ab9a2e5b7744febbfc25710700087e5295b7
Rafael Delgado <rafa12del@hotmail.com>
2021-03-08 18:57:05 +0100

    DTO implementation

---
+++ ProjetoDecode.Application/DTO/PersonDTO.cs
@@ -0,0 +1,19 @@
+﻿using ProjetoDecode.Domain.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoDecode.Application.DTO
+{
+    class PersonDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public Status Status { get; set; }
+        public ProfessionDTO Profession { get; set; }
+        public List<HobbyDTO> Hobbies { get; set; }
+    }
+}
---
+++ ProjetoDecode.Application/DTO/ProfessionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjetoDecode.Application.DTO
+{
+    class ProfessionDTO
+    {
+        public int Id { get; set; }
+        public string Role { get; set; }
+    }
+}
